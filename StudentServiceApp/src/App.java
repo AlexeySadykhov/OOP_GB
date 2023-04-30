1411db52d2b31e9e5c7c59b8d10bca2e57599f31
alex <alexeysadykhov@yahoo.com>
2023-04-30 14:27:56 +0400

    Files modified

--- StudentServiceApp/src/App.java
+++ StudentServiceApp/src/App.java
@@ -4,63 +4,23 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import Controllers.AverageAge;
+import Controllers.EmploeeController;
+import StudentDomen.Emploee;
 import StudentDomen.Student;
 import StudentDomen.StudentGroup;
-import StudentDomen.StudentSteam;
+import StudentDomen.Teacher;
 import StudentDomen.User;
 
 public class App {
-    /**
-     * Метод печати студентов в группах
-     * Если уже переопределен toString в классе StudentSteam,
-     * то можно не использовать
-     * @param steam
-     */
-    public static void printGroups(StudentSteam steam) {
-        for (StudentGroup group : steam) {
-            System.out.printf("Группа: %s\n", group.getGroupNumber());
-            for (Student student : group) {
-                System.out.println(student);
-            }
-            System.out.println("---------------");
-        }
-    }
     public static void main(String[] args) throws Exception {
-        // Создаем студентов
         // User u1 = new User("Сергей","Иванов",25);
-        Student s1 = new Student("Сергей", "Иванов", 22, (long)101);
-        Student s2 = new Student("Андрей", "Сидоров", 22, (long)111);
-        Student s3 = new Student("Иван", "Петров", 22, (long)121);
-        Student s4 = new Student("Игорь", "Иванов", 23, (long)301);
-        Student s5 = new Student("Даша", "Цветкова", 23, (long)171);
-        Student s6 = new Student("Лена", "Незабудкина", 23, (long)104);
-        Student s7 = new Student("Леонид", "Савельев", 24, (long)145);
-        // Создаем списки студентов
-        List<Student> l1 = new ArrayList<Student>();
-        l1.add(s1);
-        l1.add(s2);
-        List<Student> l2 = new ArrayList<Student>();
-        l2.add(s3);
-        l2.add(s4);
-        l2.add(s5);
-        List<Student> l3 = new ArrayList<Student>();
-        l3.add(s6);
-        l3.add(s7);
-        // Передаем списки в конструктор, создаем группы
-        StudentGroup g1 = new StudentGroup(l1);
-        StudentGroup g2 = new StudentGroup(l2);
-        StudentGroup g3 = new StudentGroup(l3);
-        // Создаем список групп
-        List<StudentGroup> groupsList = new ArrayList<StudentGroup>();
-        groupsList.add(g1);
-        groupsList.add(g2);
-        groupsList.add(g3);
-        // Создаем поток
-        StudentSteam steam = new StudentSteam(groupsList, 3);
-        printGroups(steam); // Выводим всех студентов по группам
-        Collections.sort(steam.getGroups()); // Сортируем группы
-        System.out.println("Sorted");
-        System.out.println(steam); // Выводим отсортированный поток
+        // Student s1 = new Student("Сергей", "Иванов", 22, (long)101);
+        // Student s2 = new Student("Андрей", "Сидоров", 22, (long)111);
+        // Student s3 = new Student("Иван", "Петров", 22, (long)121);
+        // Student s4 = new Student("Игорь", "Иванов", 23, (long)301);
+        // Student s5 = new Student("Даша", "Цветкова", 23, (long)171);
+        // Student s6 = new Student("Лена", "Незабудкина", 23, (long)104);
 
         // List<Student> listStud = new ArrayList<Student>();
         // listStud.add(s1);
@@ -87,7 +47,30 @@ public class App {
         // }
         
 
+        Emploee peron1 = new Emploee("Иванов", "Олег", 55, 110);
+        Student s1 = new Student("Сергей", "Иванов", 22, (long)101);
+        Teacher t1 = new Teacher("Петр", "Андреев", 51, 12, "Профессор");
 
+        //EmploeeController contrEmp = new EmploeeController();
+        EmploeeController.paySalary(peron1);
+        //contrEmp.paySalary(s1);
+
+        Integer studHour[] = {124,234,231,1,45};
+        System.out.println(EmploeeController.mean(studHour));
+
+        Double emplSalary[] = {12555.23,34213.5,10000.0};
+        System.out.println(EmploeeController.mean(emplSalary));
+
+        List<Object> users = new ArrayList();
+        users.add(peron1);
+        users.add(s1);
+        users.add(t1);
+
+        AverageAge avAg = new AverageAge<>();
+        System.out.println("Средний возраст:");
+        System.out.println(avAg.findAvg(users));
+
+        
         //System.out.println(u1);
         //System.out.println(s1);
         //System.out.println(group);
--- StudentServiceApp/src/StudentDomen/StudentGroup.java
+++ StudentServiceApp/src/StudentDomen/StudentGroup.java
@@ -3,44 +3,19 @@ package StudentDomen;
 import java.util.Iterator;
 import java.util.List;
 
-/**
- * Класс группы студентов.
- * Имплементирует Itarable и Comparable
- */
-public class StudentGroup implements Iterable<Student>, Comparable<StudentGroup> {
-    private List<Student> students; // Список студентов
-    private int groupNumber; // Номер группы
-    static int counter = 1; // Счетчик
+public class StudentGroup implements Iterable<Student> {
+    private List<Student> students;
 
     public StudentGroup(List<Student> students) {
         this.students = students;
-        this.groupNumber = counter++;
     }
-    /**
-     * Получение списка студентов
-     */
+
     public List<Student> getStudents() {
         return students;
     }
-    /**
-     * Установка списка студентов
-     * @param students
-     */
+
     public void setStudents(List<Student> students) {
         this.students = students;
-    }
-    /**
-     * Получение номера группы
-     */
-    public int getGroupNumber() {
-        return groupNumber;
-    }
-    /**
-     * Изменение номера группы
-     * @param value
-     */
-    public void setGroupNumber(int value) {
-        this.groupNumber = value;
     }
 
     // @Override
@@ -48,9 +23,6 @@ public class StudentGroup implements Iterable<Student>, Comparable<StudentGroup>
     //     return new StudentGroupIterator(students);
     // }
 
-    /**
-     * Переопределение итератора
-     */
     @Override
     public Iterator<Student> iterator() {
         return new Iterator<Student>(){
@@ -70,43 +42,6 @@ public class StudentGroup implements Iterable<Student>, Comparable<StudentGroup>
             }
 
         };
-    }
-    /**
-     * Переопределение компаратора
-     */
-    @Override
-    public int compareTo(StudentGroup o) {
-        if (this.students.size() == o.students.size()) {
-            return 0;
-        }
-        if (this.students.size() < o.students.size()) {
-            return -1;
-        }
-        else return 1;
-    }
-    // @Override
-    // public int compareTo(StudentGroup o) {
-    //     if (this.groupNumber == o.groupNumber) {
-    //         return 0;
-    //     }
-    //     if (this.groupNumber < o.groupNumber) {
-    //         return -1;
-    //     }
-    //     else return 1;
-    // }
-    /**
-     * Переопределение toString
-     */
-    @Override
-    public String toString() {
-        StringBuilder strbld = new StringBuilder();
-        for (Student student : this.students) {
-            strbld.append(student.toString());
-            strbld.append("\n");
-        }
-        String result = String.format("Номер группы: %s\nКол-во студентов: %s\n", 
-        this.groupNumber, this.students.size());
-        return result + strbld.toString();
     }
     
 }
